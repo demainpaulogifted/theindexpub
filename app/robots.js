86ff561420a8b90e9424535d271b8276ce409299
demainpaulogifted <demainpaulogifted@outlook.com>
2026-08-31 20:16:20 +0100

    Update robots.js

--- app/robots.js
+++ app/robots.js
@@ -1,7 +1,7 @@
 export default function robots() {
   const siteUrl =
     process.env.NEXT_PUBLIC_SITE_URL ||
-    "http://localhost:3000";
+    "https://theindex.name.ng";
 
   return {
     rules: {
@@ -9,6 +9,6 @@ export default function robots() {
       allow: "/",
       disallow: ["/api/", "/admin/"]
     },
-    sitemap: `${siteUrl}/sitemap.xml`
+    sitemap: `${siteUrl.replace(/\/+$/, "")}/sitemap.xml`
   };
 }
